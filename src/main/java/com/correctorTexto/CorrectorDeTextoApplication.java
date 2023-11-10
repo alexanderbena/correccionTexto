74175a3dd111689e6217fc103d0e21c128270541
charl <charl@ALEXANDERBENA-1>
2023-11-10 01:03:35 -0600

    Ejemplo 2 corrección

---
+++ src/main/java/com/correctorTexto/CorrectorDeTextoApplication.java
@@ -0,0 +1,13 @@
+package com.correctorTexto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrectorDeTextoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrectorDeTextoApplication.class, args);
+	}
+
+}
